49a8ed55dad64e67def333b24d3eaa0477bf4f2f
Jakub Rysanek <jakub.rysanek@email.cz>
2023-03-20 11:44:12 +0100

    test

--- src/vrhcaby.py
+++ src/vrhcaby.py
@@ -1,6 +1,7 @@
-def main():
-    ...
+def starthry():
+    cerne_kameny = [Kamen() for i in range(10)]
+    bile_kameny = [Kamen() for i in range(10)]
 
 if __name__ == "__main__":
-    main()
+    starthry()
 
