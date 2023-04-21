457ae3672f113c958666be7f28f03d432391f795
ondra <ondra.thomas@seznam.cz>
2023-04-22 00:18:57 +0200

    pridany nejake detaily

--- src/vrhcaby.py
+++ src/vrhcaby.py
@@ -1,11 +1,10 @@
-import random
-import os
 import json
+import os
 
 # zajistuje barvy textu
 os.system("")
 
-class style():
+class style:
     BLACK = '\033[30m'
     RED = '\033[31m'
     GREEN = '\033[32m'
@@ -19,19 +18,20 @@ class style():
     RESET = '\033[0m'
 
     # vycisti konzoli
+    @staticmethod
     def clear():
         os.system("cls")
 
 
 
 class Game:
-    
+
     def __init__(self, gameboard, pozice, player1, player2) -> None:
         # herni pole
         self._gameboard = gameboard
         # dvojkostka
         self._doubledice = []
-        self._spikes = [[] for j in range(24)]
+        self._spikes = [[] for _ in range(24)]
         # bar
         self._bar = ...
         self._stone = pozice
@@ -48,19 +48,19 @@ class Game:
     @doubledice.setter
     def doubledice(self, value):
         self._doubledice = value
-    
+
     @property
     def gameboard(self):
         return self._gameboard
-    
+
     @property
     def spikes(self):
         return self._spikes
-    
+
     @property
     def last_command(self):
         return self._last_command
-    
+
     @last_command.setter
     def last_command(self, value):
         self._last_command = value
@@ -68,7 +68,7 @@ class Game:
     @property
     def turn(self):
         return self._turn
-    
+
     @turn.setter
     def turn(self, value):
         self._turn = value
@@ -77,10 +77,11 @@ class Game:
         """
         Funkce bude přičítat kola a měnit hráče na tahu.
         """
-        
+
         ...
 
-    def throw_dice(self, dice:list) -> None:
+    @staticmethod
+    def throw_dice(dice:list) -> list:
         dice.clear()
         hod1, hod2 = random.randint(1, 6), random.randint(1, 6)
         # kontrola hozenych hodnot
@@ -93,9 +94,10 @@ class Game:
             for _ in range(4):
                 dice.append(str(hod1))
             return dice
-    
-    def spike_occupancy(self, spike_list:list) -> str:
-        
+
+    @staticmethod
+    def spike_occupancy(spike_list:list) -> str:
+
         # nedokonceny system
         # vypisuje obsazenost spiku a zajistuje formatovani
         if 0 <= len(spike_list) < 10:
@@ -112,7 +114,7 @@ class Game:
             spaces = 156*" "
         else:
             spaces = 150*" "
-        
+
         # tvorba cislovani spiku
         spike_row1 = ([str(_) for _ in range(1,7)], [str(_) for _ in range(7,10)], [str(_) for _ in range(10,13)])
         spike_row2 = ([str(_) for _ in range(13,19)], [str(_) for _ in range(19, 25)])
@@ -161,7 +163,7 @@ class Game:
         command = command.lower()
         with open(cfg, 'r') as config_file:
             all_commands = json.load(config_file)['commands']
-        
+
         if command in all_commands:
             if command == "presun":
                 command = f"{style.CYAN}Prikaz \'{command}\' zatím nemá zatím implementovanou funkci{style.RESET}"
@@ -174,13 +176,15 @@ class Game:
 
 class Menu:
     def __init__(self, options, config) -> None:
+        self._player1 = None
+        self._player2 = None
         self._options = options
         self._conf = config
 
     @property
     def self_options(self):
         return self._options
-    
+
     @property
     def self_conf(self):
         return self._conf
@@ -199,77 +203,190 @@ class Menu:
                 - cela hra se vypne 
     """
 
+    @property
     def game_setup(self):
-        
+
         # volba PVP, PVE
         print(" VITEJTE VE HRE VRHCABY! \n      MOZNOSTI HRY        \n       PvE    PvP\n")
-        
+
         while True:
             volba = input("vase volba : ")
             volba.lower()
-            
+
             if volba not in ["pve", "pvp"]:
                 print("Tento mod neni v nabidce.")
             else:
                 print(f"Vybrali jste mod : {volba}\n")
                 break
-        
+
         # zmena jmena
         def zmena_jmena(i: int):
-                
+
             while True:
-                max_delka = 10
+                max_delka: int = 10
                 print(f"\nZadejte jmeno pro hrace ({i}) | delka jmena 3 - 10")
                 vybrane_jmeno = input("Zvolene jmeno: ")
-                        
+
                 if len(vybrane_jmeno) < 3 or len(vybrane_jmeno) > 10:
                     print("Jmeno nesplnuje podminky!")
 
                 else:
                     return vybrane_jmeno
-        
-        def nastaveni_barvy(barvy: list, i: int):
+
+        def nastaveni_barvy(barvy: list):
             while True:
                 print(f"\nVyberte barvu z nasledujicich: {barvy}")
                 vybrana_barva = input("Zvolena barva: ")
-                
+
                 if vybrana_barva in barvy:
                     barvy.remove(vybrana_barva)
                     return vybrana_barva, barvy
-                
+
                 else:
                     print("Tato barva se nenachazi v moznostech!")
-                    
-    
+
+
         # volba jmen PVP
         if volba == "pvp":
             barvy = ["a", "b", "c", "d"]                          # zatim orientacne, jen potreba doplnit barvy
-            
+
             self._player1 = zmena_jmena(1)
             #self._player1_barvy = nastaveni_barvy(barvy, 1)
             self._player2 = zmena_jmena(2)
             #self._player2_barvy = nastaveni_barvy(barvy, 2)
-            
-        
+
+
         # volba jmen PvE
         if volba == "pve":
-            
+
             barvy = ["a", "b", "c", "d"]                          # zatim orientacne, jen potreba doplnit barvy
             self._player1 = zmena_jmena(1)
             #self._player1_barvy = nastaveni_barvy(barvy, 1)
             self._player2 = "AI"
             #self._player2_barvy = nastaveni_barvy(barvy, 2)
 
-    def load():
+    def load(self):
         # v pripade vyberu moznosti nacist hru
         ...
 
-    def play():
+    def play(self):
         # presun do dalsi casti menu, moznosti budou nova hra a nacit hru
         ...
 
+    @staticmethod
     def quit_game():
         quit()
+
+
+# kostky
+import random
+class Dicebasket:
+    def __init__(self, dicecount):
+        self.dicecount = dicecount
+        self. dicevalue = []
+
+    def throw(self):
+        for i in range(self.dicecount):
+            self.dicevalue.append(random.randirt(1, 6))
+        if len(set(self.dicevalue)) == 1:
+            for i in range(self.dicecount):
+                self.dicevalue.append(self.dicevalue[0])
+    def __repr__(self):
+        pass
+
+    kostky = Dicebasket(2)
+    kostky.throw()
+    print(kostky.dicevalue)
+
+    #gameboard(hrací deska)
+
+import Queue_class
+class Gameboard:
+    def __init__(self):
+        self.gameboard1 = Queue_class.Zasobnik(None)
+        self.gameboard2 = Queue_class.Zasobnik(None)
+        self.gameboard3 = Queue_class.Zasobnik(None)
+        self.gameboard4 = Queue_class.Zasobnik(None)
+        self.gameboard5 = Queue_class.Zasobnik(None)
+        self.gameboard6 = Queue_class.Zasobnik(None)
+        self.gameboard7 = Queue_class.Zasobnik(None)
+        self.gameboard8 = Queue_class.Zasobnik(None)
+        self.gameboard9 = Queue_class.Zasobnik(None)
+        self.gameboard10 = Queue_class.Zasobnik(None)
+        self.gameboard11 = Queue_class.Zasobnik(None)
+        self.gameboard12 = Queue_class.Zasobnik(None)
+        self.gameboard13 = Queue_class.Zasobnik(None)
+        self.gameboard14 = Queue_class.Zasobnik(None)
+        self.gameboard15 = Queue_class.Zasobnik(None)
+        self.gameboard16 = Queue_class.Zasobnik(None)
+        self.gameboard17 = Queue_class.Zasobnik(None)
+        self.gameboard18 = Queue_class.Zasobnik(None)
+        self.gameboard19 = Queue_class.Zasobnik(None)
+        self.gameboard20 = Queue_class.Zasobnik(None)
+        self.gameboard21 = Queue_class.Zasobnik(None)
+        self.gameboard22 = Queue_class.Zasobnik(None)
+        self.gameboard23 = Queue_class.Zasobnik(None)
+        self.gameboard24 = Queue_class.Zasobnik(None)
+        self.whitebar = Queue_class.Zasobnik(None)
+        self.blackbar = Queue_class.Zasobnik(None)
+        self.whitehouse = Queue_class.Zasobnik(None)
+        self.blackhouse = Queue_class.Zasobnik(None)
+
+        self.gameboard_directionary = {
+            0: self.gameboard1,
+            1: self.gameboard2,
+            2: self.gameboard3,
+            3: self.gameboard4,
+            4: self.gameboard5,
+            5: self.gameboard6,
+            6: self.gameboard7,
+            7: self.gameboard8,
+            8: self.gameboard9,
+            9: self.gameboard10,
+            10: self.gameboard11,
+            11: self.gameboard12,
+            12: self.gameboard13,
+            13: self.gameboard14,
+            14: self.gameboard15,
+            15: self.gameboard16,
+            16: self.gameboard17,
+            17: self.gameboard18,
+            18: self.gameboard19,
+            19: self.gameboard20,
+            20: self.gameboard21,
+            21: self.gameboard22,
+            22: self.gameboard23,
+            23: self.gameboard24,
+            24: self.whitebar,
+            25: self.blackbar,
+            26: self.whitehouse,
+            27: self.blackhouse
+        }
+        for i in range(5):
+            self.gameboard_dictioanary[5].add_item("O")
+            self.gameboard_dictioanary[12].add_item("O")
+            self.gameboard_dictioanary[11].add_item("X")
+            self.gameboard_dictioanary[18].add_item("X")
+        for i in range(2):
+            self.gameboard_dictioanary[0].add_item("X")
+            self.gameboard_dictioanary[23].add_item("O")
+        for i in range(3):
+            self.gameboard_dictioanary[7].add_item("O")
+            self.gameboard_dictioanary[16].add_item("X")
+    def print_current_state(self):
+        for key in self.gameboard_directionary.keys():
+            print("pole", key+1, ": ", self.gameboard_directionary[key])
+    def __repr__(self):
+        pass
+    def move_stone(self, frompos, topos):
+        pass
+    def get_position(self):
+        pass
+
+pole = Gameboard()
+print(pole.print_current_state())
+
+
 
 
 def main():
@@ -288,8 +405,9 @@ def main():
         print(style.GREEN + "Made by: Jakub Ryšánek, Ondřej Thomas, Jakub Kepič" + style.RESET)
         cmd_line = input("> ")
         game1.command_detection(cmd_line, config_file)
-    
-        
+
+
+
 
 if __name__ == "__main__":
     main()
