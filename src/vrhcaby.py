27feddbeeb6b2b0c82b590b46f845d6f85a3846f
Jakub Kepi <121820974+KKepi@users.noreply.github.com>
2023-05-04 19:53:37 +0200

    Smer pro hrace 2, ai
    Hrac 2 a ai maji smer z 24 pozice na 0 pozici

--- src/vrhcaby.py
+++ src/vrhcaby.py
@@ -303,33 +303,51 @@ class HerniPole:
             if len(self.spike_list[i]["kameny"]) > 0 and self.spike_list[i]["barva"] == barva_hrace1:    # pokud v spiku je nejaky kamen a ma barvu hrace zapise se do moznych tahu
                 podezrele_dostupne_tahy.append(i)                                                        # index dostupneho kamene ve spiku self.spike_list[i]
             
-                                                                                                         # dostupne_tahy
-                                                                                                         # kontrola spiku v pripade kdyby chtel hrac posunout sutr (aby na tomto spiku nebyl jiny hrac.... pokud by byl a mel by 1 sutr pak ho muze vyhodit)
+                # dostupne_tahy
+                # kontrola spiku v pripade kdyby chtel hrac posunout sutr (aby na tomto spiku nebyl jiny hrac.... pokud by byl a mel by 1 sutr pak ho muze vyhodit)
             
         print(self.spike_list)
             
         tahy = [] # dvojce .... (Sx, Sy)
-        for i in podezrele_dostupne_tahy:
-            if i+hod_kostkou < 24:
+        
+        if id_hrace == 1:
+            for i in podezrele_dostupne_tahy:
+                if i+hod_kostkou < 24:
                     
                     # kontrola poctu kamenu mozneho tahu.... pripsani na seznam dostupnych tahu
-                if len(self.spike_list[i+hod_kostkou]["kameny"]) == 0:
-                                # Sx , Sy
-                    tahy.append((i,i+hod_kostkou))
+                    if len(self.spike_list[i+hod_kostkou]["kameny"]) == 0:
+                                        # Sx , Sy
+                        tahy.append((i,i+hod_kostkou))
                     
                     
-                if len(self.spike_list[i+hod_kostkou]["kameny"]) == 1 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace2:
-                    tahy.append((i,i+hod_kostkou))
+                    if len(self.spike_list[i+hod_kostkou]["kameny"]) == 1 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace2:
+                        tahy.append((i,i+hod_kostkou))
                     
-                if len(self.spike_list[i+hod_kostkou]["kameny"]) > 0 and len(self.spike_list[i+hod_kostkou]["kameny"]) < 5 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace1:
-                    tahy.append((i,i+hod_kostkou))
+                    if len(self.spike_list[i+hod_kostkou]["kameny"]) > 0 and len(self.spike_list[i+hod_kostkou]["kameny"]) < 5 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace1:
+                        tahy.append((i,i+hod_kostkou))
+        
+        else: # id_hrace == 2 , id_hrace == 3
+            for i in podezrele_dostupne_tahy:
+                if i-hod_kostkou >= 0:
+                    
+                    # kontrola poctu kamenu mozneho tahu.... pripsani na seznam dostupnych tahu
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) == 0:
+                                    # Sx , Sy
+                        tahy.append((i,i-hod_kostkou))
+                    
+                    
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) == 1 and self.spike_list[i-hod_kostkou]["barva"] == barva_hrace2:
+                        tahy.append((i,i-hod_kostkou))
+                    
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) > 0 and len(self.spike_list[i-hod_kostkou]["kameny"]) < 5 and self.spike_list[i-hod_kostkou]["barva"] == barva_hrace1:
+                        tahy.append((i,i-hod_kostkou))
             
             
         # hracova volba tahu
             
         print(f"Hrac: {id_hrace}\nHodil kostkou: {hod_kostkou}")
         print(f"K posunuti jsou nasledujici kombinace:")
-                
+            
         kombinace = 1                                               # index zacina od 1 bude treba ho zmensit pokud se s nim bude pracovat jako s prvnim prvkem v seznamu
         platne_kombinace = []
         for sx, sy in tahy:
@@ -338,23 +356,42 @@ class HerniPole:
             kombinace += 1
             
         # vyber
-            
-        while True:
-            volba = input("Pro posunuti zadejte cislo v zavorkach: ")
+        
+        if id_hrace == 1:    
+            while True:
+                volba = input("Pro posunuti zadejte cislo v zavorkach: ")
                 
-            spiky_k_presunuti = [tahy[int(volba)-1][0], tahy[int(volba)-1][1]]
+                spiky_k_presunuti = [tahy[int(volba)-1][0], tahy[int(volba)-1][1]]
                 
-            if volba.isnumeric() == True:
-                volba = int(volba)
-                if volba in platne_kombinace:
-                    temp = volba-1              # zmensen o 1 aby se s nim dalo pocitat jako s opravdovym prvkem v seznamu.... len(x) != indexy x.... len(x) + 1 == indexy x
-                        
-                    print(f"Vybral jste kombinaci {volba} a posouvate S{tahy[temp][0]} na S{tahy[temp][1]}")
-                    break
+                if volba.isnumeric() == True:
+                    volba = int(volba)
+                    if volba in platne_kombinace:
+                        temp = volba-1              # zmensen o 1 aby se s nim dalo pocitat jako s opravdovym prvkem v seznamu.... len(x) != indexy x.... len(x) + 1 == indexy x
+                    
+                        print(f"Vybral jste kombinaci {volba} a posouvate S{tahy[temp][0]} na S{tahy[temp][1]}")
+                        break
+                    else:
+                        print("Vase volba se nenachazi v moznostech")
                 else:
-                    print("Vase volba se nenachazi v moznostech")
-            else:
-                print("zadejte cislo")
+                    print("zadejte cislo")
+        
+        else: # id_hrace == 2 , id_hrace == 3
+            while True:
+                volba = input("Pro posunuti zadejte cislo v zavorkach: ")
+                
+                spiky_k_presunuti = [tahy[int(volba)-1][0], tahy[int(volba)-1][1]]
+                
+                if volba.isnumeric() == True:
+                    volba = int(volba)
+                    if volba in platne_kombinace:
+                        temp = volba-1              # zmensen o 1 aby se s nim dalo pocitat jako s opravdovym prvkem v seznamu.... len(x) != indexy x.... len(x) + 1 == indexy x
+                        
+                        print(f"Vybral jste kombinaci {volba} a posouvate S{tahy[temp][1]} na S{tahy[temp][0]}")
+                        break
+                    else:
+                        print("Vase volba se nenachazi v moznostech")
+                else:
+                    print("zadejte cislo")
             
         ########
         # presunuti
@@ -421,19 +458,19 @@ class HerniPole:
             
             tahy = [] # dvojce .... (Sx, Sy)
             for i in podezrele_dostupne_tahy:
-                if i+hod_kostkou < 24:
+                if i-hod_kostkou >= 0:
                     
                        # kontrola poctu kamenu mozneho tahu.... pripsani na seznam dostupnych tahu
-                    if len(self.spike_list[i+hod_kostkou]["kameny"]) == 0:
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) == 0:
                                     # Sx , Sy
-                        tahy.append((i,i+hod_kostkou))
+                        tahy.append((i,i-hod_kostkou))
                     
                     
-                    if len(self.spike_list[i+hod_kostkou]["kameny"]) == 1 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace1:
-                        tahy.append((i,i+hod_kostkou))
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) == 1 and self.spike_list[i-hod_kostkou]["barva"] == barva_hrace1:
+                        tahy.append((i,i-hod_kostkou))
                     
-                    if len(self.spike_list[i+hod_kostkou]["kameny"]) > 0 and len(self.spike_list[i+hod_kostkou]["kameny"]) < 5 and self.spike_list[i+hod_kostkou]["barva"] == barva_hrace3:
-                        tahy.append((i,i+hod_kostkou))
+                    if len(self.spike_list[i-hod_kostkou]["kameny"]) > 0 and len(self.spike_list[i-hod_kostkou]["kameny"]) < 5 and self.spike_list[i-hod_kostkou]["barva"] == barva_hrace3:
+                        tahy.append((i,i-hod_kostkou))
             
             
             # ai vytvoreni voleb
@@ -492,7 +529,7 @@ class HerniPole:
                 self.spike_list[spiky_k_presunuti[0]]["barva"] = None            # barva puvodniho spiku se zmeni na None
                 self.spike_list[spiky_k_presunuti[1]]["barva"] = barva_hrace3    # barva se prida na cilovy spike
             
-            print(self.spike_list)               
+            print(self.spike_list) 
 
 
 # kamen = Kamen(0)
