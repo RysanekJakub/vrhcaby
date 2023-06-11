7a8e5497ea6afd1b7945aff8fefd6243386e7194
Jakub Kepi <121820974+KKepi@users.noreply.github.com>
2023-06-11 19:06:18 +0200

    Update herni_pole.py
    Herni_pole by melo byt pouzitelne, funguje presouvani kamenu, neztraci se, kod zkracen a zefektivnen

--- src/herni_pole.py
+++ src/herni_pole.py
@@ -1,4 +1,4 @@
-import random
+
 
 
 class Kamen:
@@ -10,184 +10,157 @@ class Kamen:
     def zmena(self, index):
         self.pamet.append(index)
         
-        
 class Hernipole:
     
     def __init__(self):
         self.spikes = []
-
-    
-    def nacteni_spiku(self):
-        for i in range(24):
-            self.spikes.append({"kameny": [], "barva": None})
-            
-            if i == 0:
-                sutr = Kamen("cerna")
-                sutr.zmena(i)
-                for j in range(2):
-                    self.spikes[i]["kameny"].append(sutr)
-                    
-                self.spikes[i]["barva"] = "cerna"
-            
-            if i == 5:
-                sutr = Kamen("bila")
-                sutr.zmena(i)
-                for j in range(5):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "bila"
-                    
-            if i == 7:
-                sutr = Kamen("bila")
-                sutr.zmena(i)
-                for j in range(3):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "bila"
-            
-            if i == 11:
-                sutr = Kamen("cerna")
-                sutr.zmena(i)
-                for j in range(5):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "cerna"
-            
-            if i == 12:
-                sutr = Kamen("bila")
-                sutr.zmena(i)
-                for j in range(5):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "bila"
-                    
-            if i == 16:
-                sutr = Kamen("cerna")
-                sutr.zmena(i)
-                for j in range(3):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "cerna"
-            
-            if i == 18:
-                sutr = Kamen("cerna")
-                sutr.zmena(i)
-                for j in range(5):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "cerna"
-            
-            if i == 23:
-                sutr = Kamen("bila")
-                sutr.zmena(i)
-                for j in range(2):
-                    self.spikes[i]["kameny"].append(sutr)
-                
-                self.spikes[i]["barva"] = "bila"
-    
-    def tah(self, id_hrace, hod_kostkou):
         
-        # id_hrac = 1 ..... bila
-        global start, end
-        if id_hrace == 1:
+    def vytvorit_pole(self):
+        for i in range(24):
+            self.spikes.append({
+                "kameny":[],
+                "barva":None
+            })
+#                indx   barva   pocet
+        rozdani = [(0, "cerna", 2), (5, "bila", 5), (7, "bila", 3), (11, "cerna", 5), (13, "bila", 5), (16, "cerna", 3), (19, "cerna", 5), (23, "bila", 2)]
+        
+        for indx, barva, pocet in rozdani:
+            self.spikes[indx]["barva"] = barva
+            for i in range(pocet):
+                sutr = Kamen(barva)
+                self.spikes[indx]["kameny"].append(sutr)
+                
+#                  bila   /   cerna
+    def tah(self, barva_hrace, hod_kostkou):
+        
+        kameny_ze_hry = []
+        
+        if barva_hrace == "bila":
+            parametr = -1
             
-            # zkoumani podezrelych tahu
-            mozne_tahy = []
-            for i in range(len(self.spikes)):
-                if self.spikes[i]["barva"] == "bila" and self.spikes[i] != 23:               # spike s barvou hrace
-                    
-                    # spike na ktery presouvame nema barvu
-                    if self.spikes[i-hod_kostkou]["barva"] == None:                        
-                        mozne_tahy.append((i, i-hod_kostkou, "prazdno"))
-                        
-                    # spike na ktery presouvame ma barvu protivnika a je jen jeden kamen
-                    if self.spikes[i-hod_kostkou]["barva"] == "cerna" and len(self.spikes[i-hod_kostkou]["kameny"]) == 1:
-                        mozne_tahy.append((i, i-hod_kostkou, "vyhozeni"))
-                        
-                    # spike na ktery presouvame ma barvu hrace
-                    if self.spikes[i-hod_kostkou]["barva"] == "bila" and len(self.spikes[i-hod_kostkou]["kameny"]) > 0:
-                        mozne_tahy.append((i, i-hod_kostkou, "pridani"))
+        if barva_hrace == "cerna":
+            parametr = 1
             
-            # vyber z podezrelych tahu
-            while True:
-                i = 0
-                print("--------------------------")
-                print("VYBERTE SI TAH")
-                print("--------------------------")
-                for start, end, druh in mozne_tahy:
-                    print(f"({i}) kamen z S{start+1} na S{end+1} - {druh}")
-                    i += 1
-                
-                while True:
-                    print("--------------------------")
-                    volba = input("vyberte z voleb: ")
-                    
-                    if volba.isdigit() == True:
-                        volba = int(volba)
-                        
-                        if volba >= 0 and volba <= i:
-                            print("--------------------------")
-                            print(f"vybrali jste: {volba}")
-                            
-                            klic = (mozne_tahy[volba][0], mozne_tahy[volba][1], mozne_tahy[volba][2])
-                            
-                            break
-                        
-                        else:
-                            print("tato volba neni v moznostech")
-                            
-                    else:
-                        print("zadejte cislo")
-                
-                break
+        mozne_tahy = []
+        
+        for i in range(len(self.spikes)):
             
-            # indexy  start    end     druh
-            #           0      1      2
-            #
-            # klic odkazuje na mozne_tahy .... lepsi prehlednost
-            #
-            # po presunu nezbyde kamen na start spiku
-            if klic[2] == "prazdno" and len(self.spikes[start]["kameny"]) == 1:
-                presouvany_kamen = self.spikes[start]["kameny"][-1]                   # vybrani presouvaneho kamene
-                presouvany_kamen.pamet.append(end)                                    # pridani nove pozice do pameti
-                del self.spikes[start]["kameny"][-1]                                  # smazani z puvodniho spiku
-                self.spikes[end]["kameny"].append(presouvany_kamen)                   # pridani na novy spike
-                self.spikes[start]["barva"] = None                                    # puvodni spike ma barvu None pro prehlednost
-                
-                self.spikes[end]["barva"] = "bila"                                    # nastaveni nove barvy
-            
-            # po presunu zbyde kamen na start spiku
-            if klic[2] == "prazdno" and len(self.spikes[start]["kameny"]) > 1:
-                presouvany_kamen = self.spikes[start]["kameny"][-1]                   # vybrani presouvaneho kamene
-                presouvany_kamen.pamet.append(end)                                    # pridani nove pozice do pameti
-                del self.spikes[start]["kameny"][-1]                                  # smazani z puvodniho spiku
-                self.spikes[end]["kameny"].append(presouvany_kamen)                   # pridani na novy spike
-                self.spikes[start]["barva"] = None                                    # puvodni spike ma barvu None pro prehlednost
-                
-                self.spikes[end]["barva"] = "bila"                                    # nastaveni nove barvy
-                
-            # po presunu vyhodim kamen protivnika a na startu nezbyde kamen
-            if klic[2] == "vyhozeni" and len(self.spikes[start]["kameny"]) == 1:
-                presouvany_kamen = self.spikes[start]["kameny"][-1]                  # vybrani presouvaneho kamene
-                presouvany_kamen.pamet.append(end)                                   # pridani nove pozice do pameti
-                del self.spikes[start]["kameny"][-1]                                 # smazani z puvodniho spiku
-                self.spikes[end]["barva"] = None                                     # puvodni spike ma barvu None pro prehlednost
-                
-                
-                del self.spikes[end]["kameny"][-1]                                   # smazani protivnikova kamene
-                self.spikes[end]["kameny"].append(presouvany_kamen)                  # pridani na novy spike
-                self.spikes[end]["barva"] = "bila"                                   # nastaveni nove barvy
-            
-            # po presunu vyhodim kamen protivnika a na startu zbyde kamen
-            if klic[2] == "vyhozeni" and len(self.spikes["start"]["kameny"]) > 1:
-                presouvany_kamen = self.spikes[start]["kameny"][-1]                  # vybrani presouvaneho kamene
-                presouvany_kamen.pamet.append(end)                                   # pridani nove pozice do pameti
-                del self.spikes[start]["kameny"][-1]                                 # smazani z puvodniho spiku
-                
-                del self.spikes[end]["kameny"][-1]                                   # smazani protivnikova kamene
-                self.spikes[end]["kameny"].append(presouvany_kamen)                  # pridani na novy spike
-                self.spikes[end]["barva"] = "bila"                                   # nastaveni nove barvy
-                
-hra = Hernipole()
-hra.nacteni_spiku()
+#                 spike s barvou hrace
+            if self.spikes[i]["barva"] == barva_hrace and i+parametr*hod_kostkou >= 0:
 
+#                 parametr udava smer
+                budouci_pozice = i + parametr*hod_kostkou
+
+#                    kontrola spiku
+#
+#                    budouci spike
+#                    prazdny
+                if self.spikes[budouci_pozice]["barva"] == None:
+                    mozne_tahy.append((i, budouci_pozice))
+                    
+                if self.spikes[budouci_pozice]["barva"] != barva_hrace and len(self.spikes[budouci_pozice]["kameny"]) == 1:
+                    mozne_tahy.append((i, budouci_pozice))
+                    
+                if self.spikes[budouci_pozice]["barva"] == barva_hrace:
+                    mozne_tahy.append((i, budouci_pozice))
+        
+#                    VYBER TAHU
+        while True:
+            print("-------------------------------")
+            print("VYBERTE SI TAH")
+            print("-------------------------------")
+            
+#                    ODKAZUJICI NA INDEX V MOZNE_TAHY
+            temp = 0
+            for x1, x2 in mozne_tahy:
+                print(f"({temp}) KAMEN Z S{x1} -> S{x2}")
+                temp += 1
+            
+            volba = input("VOLIM : ")
+            if volba.isdigit() == True:
+                volba = int(volba)
+                if volba >= 0 and volba <= temp:
+                    klic = (mozne_tahy[volba][0], mozne_tahy[volba][1])
+                    break
+            else:
+                print("neplatna volba")
+
+
+#            start x1         end x2
+#            klic[0]          klic[1]
+#
+#   presun na prazdny spike, nezbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) == 0 and len(self.spikes[klic[0]]["kameny"]) == 1:#  
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            self.spikes[klic[1]]["barva"] = barva_hrace#                   pridani barvy na spike
+            self.spikes[klic[0]]["barva"] = None#                          smazani barvy z puvodniho
+            return
+            
+#   presun na prazdny spike, zbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) == 0 and len(self.spikes[klic[0]]["kameny"]) > 1:#
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            self.spikes[klic[1]]["barva"] = barva_hrace#                   pridani barvy na spike
+            return
+
+#   presun na spike s jednim kamenem protivnika, nezbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) == 1 and self.spikes[klic[1]]["barva"] != barva_hrace and len(self.spikes[klic[0]]["kameny"]) == 1:#
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            self.spikes[klic[0]]["barva"] = None#                          smazani barvy z puvodniho
+            
+            kameny_ze_hry.append(self.spikes[klic[1]]["kameny"][-1])#      pridani kamene vylouceneho ze hry do historie vyhozu
+            del self.spikes[klic[1]]["kameny"][-1]#                        smazani protivnika
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            self.spikes[klic[1]]["barva"] = barva_hrace#                   pridani barvy na spike
+            return
+            
+#   presun na spike s jednim kamenem protivnika, zbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) == 1 and len(self.spikes[klic[0]]["kameny"]) > 1:#
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            
+            kameny_ze_hry.append(self.spikes[klic[1]]["kameny"][-1])#      pridani kamene vylouceneho ze hry do historie vyhozu
+            del self.spikes[klic[1]]["kameny"][-1]#                        smazani protivnika
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            self.spikes[klic[1]]["barva"] = barva_hrace#                   pridani barvy na spike
+            return
+            
+#   presun na spike se stejnou barvou, nezbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) >= 1 and self.spikes[klic[1]]["barva"] == barva_hrace and len(self.spikes[klic[0]]["kameny"]) == 1:#  
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            self.spikes[klic[0]]["barva"] = None#                          smazani barvy z puvodniho
+            return
+
+#   presun na spike se stejnou barvou, zbyde sutr na puvodnim
+        if len(self.spikes[klic[1]]["kameny"]) >= 1 and self.spikes[klic[1]]["barva"] == barva_hrace and len(self.spikes[klic[0]]["kameny"]) > 1:#  
+            presouvany_kamen = self.spikes[klic[0]]["kameny"][-1]#         presouvany kamen
+            presouvany_kamen.pamet.append(klic[1])#                        pridani do pameti
+            del self.spikes[klic[0]]["kameny"][-1]#                        smazani z puvodniho spiku
+            self.spikes[klic[1]]["kameny"].append(presouvany_kamen)#       pridani na spike
+            return
+            
+hra1 = Hernipole()
+hra1.vytvorit_pole()
+
+indx = 0
+for spike in hra1.spikes:
+    print(spike, indx, len(spike["kameny"]))
+    indx += 1
+
+hra1.tah("bila", 7)
+
+indx = 0
+for spike in hra1.spikes:
+    print(spike, indx, len(spike["kameny"]))
+    indx += 1
